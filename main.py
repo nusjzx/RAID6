23ee1b3bfe05dfee23cec2350d3a57fa1a696fbe
Sanat Mharolkar <sanatmharolkar@gmail.com>
2023-11-23 19:25:11 +0800

    added multiple test files

--- main.py
+++ main.py
@@ -1,21 +1,108 @@
+import random
+import time
 import raid6
-from random import randrange
+from matplotlib import pyplot as plt
+from PIL import Image
+import io
 
 if __name__ == "__main__":
-    storage = raid6.RAID6('./nodes', 4, 2, 8)
-    files = ['test_small.txt', 'test_middle.txt']
+    # Initialize RAID6 with 6 nodes/disks (2 parities) and block size of 8 bytes
+    storage = raid6.RAID6('./nodes', 4, 2, 16)
+    # This class supports any other configurations too, for example:
+    # storage = raid6.RAID6('./nodes', 6, 2, 32)
+    files = []
+    
+    ### TEST FILE 1
+    print("Test 1: Short text file")
+    file_name = "short_text.txt"
+    files.append(file_name)
+    with open(file_name, 'rb') as f:
+        data = f.read()
+        storage.write(file_name, data)
+    
+    result = storage.retrieve(file_name)
+    print(result)
 
-    for file_name in files:
-        with open(file_name, 'rb') as f:
-            data = f.read()
-            storage.write(file_name, data)
-
-    # # Random Disk Failure and Detection
-    # print("\nSimulating Disk Failures and Detecting...")
-    failed_node_block_1 = storage.fail_disk(0, 2)
-    failed_node_block_2 = storage.fail_disk(2, 3)
+    # Random Disk Failures and Detection
+    print("Simulating random disk failures...")
+    fail_ids = random.sample(range(6), 2)
+    failed_node_block_1 = storage.fail_node(fail_ids[0])
+    failed_node_block_2 = storage.fail_node(fail_ids[1])
+    
+    print("Detecting disk failures and reconstructing missing data...")
     storage.handle_disk_failure()
 
-    for file_name in files:
-        result = storage.retrieve(file_name)
+    # Retrieve file to verify reconstructed contents
+    print("Retrieving reconstructed file...\n--")
+    result = storage.retrieve(file_name)
+    print(result)
+    print("---------------------------")
+
+    # ------------------------------------------------------------------------- #
+
+    ### ADDING TEST FILE 2 TO STORAGE
+    print("Test 2: Long text file")
+    file_name = "long_text.txt"
+    files.append(file_name)
+    with open(file_name, 'rb') as f:
+        data = f.read()
+        storage.write(file_name, data)
+    
+    result = storage.retrieve(file_name)
+    print(result)
+
+    # Random Disk Failures and Detection
+    print("Simulating random disk failures...")
+    fail_ids = random.sample(range(6), 2)
+    failed_node_block_1 = storage.fail_node(fail_ids[0])
+    failed_node_block_2 = storage.fail_node(fail_ids[1])
+    
+    print("Detecting disk failures and reconstructing missing data...")
+    storage.handle_disk_failure()
+
+    # Retrieve files to verify reconstructed contents
+    print("Retrieving reconstructed files...\n--")
+    for file in files:
+        result = storage.retrieve(file)
         print(result)
+        print("--")
+    print("---------------------------")
+
+    # ------------------------------------------------------------------------- #
+
+    ### ADDING TEST FILE 3 TO STORAGE
+    print("Test 2: Image file")
+    file_name = "lena.jpg"
+    files.append(file_name)
+    with open(file_name, 'rb') as f:
+        data = f.read()
+        storage.write(file_name, data)
+    
+    result = storage.retrieve(file_name)
+    image = Image.open(io.BytesIO(result))
+    image.show()
+
+    # Random Disk Failures and Detection
+    print("Simulating random disk failures...")
+    fail_ids = random.sample(range(6), 2)
+    failed_node_block_1 = storage.fail_node(fail_ids[0])
+    failed_node_block_2 = storage.fail_node(fail_ids[1])
+    
+    print("Detecting disk failures and reconstructing missing data...")
+    storage.handle_disk_failure()
+
+    # Retrieve files to verify reconstructed contents
+    print("Retrieving reconstructed files...\n--")
+    # Using this variable i to separate image file from the texts, since we don't have any file type detection in our implementation
+    i = 0
+    for file in files:
+        result = storage.retrieve(file)
+        if i < 2:
+            print(result)
+        else:
+            result = storage.retrieve(file)
+            image = Image.open(io.BytesIO(result))
+            image.show()
+        print("--")
+        i += 1
+    print("---------------------------")
--- raid6.py
+++ raid6.py
@@ -69,52 +69,6 @@ class RAID6():
         parities = self.gf.matmul(self.gf.vander, splits)
         return np.concatenate([splits, parities], axis=0)
 
-    
-    def fail_disk(self, node_id, block_id):
-        # introduce node and block failure by deleting the block file
-        file_name = os.path.join(self.path, 'node{}'.format(node_id), 'block{}'.format(block_id))
-        if os.path.exists(file_name):
-            os.remove(file_name)
-            print("block {} in Node {} failed".format(block_id, node_id))
-            return block_id, node_id
-        else:
-            print("block {} in Node {} not found".format(block_id, node_id))
-            return None
-
-    
-    def handle_disk_failure(self):
-        # detect which node is corrupted
-        for stripe_i in range(self.cur_stripe_index):
-            normal_data = []
-            failed_i = []
-            for i in range(self.k + self.m):
-                roll_i = (i - stripe_i) % (self.k + self.m)
-                file_name = os.path.join(self.path, 'node{}'.format(roll_i), 'block{}'.format(stripe_i))
-                if not os.path.exists(file_name):
-                    print("Detected failure: block {} in Node {}".format(stripe_i, roll_i))
-                    failed_i.append(i)
-                else:
-                    with open(self.path + '/node{}'.format(roll_i) + '/block{}'.format(stripe_i), 'rb') as f:
-                        block = np.asarray(bytearray(f.read()), dtype=np.uint8)
-                        normal_data.append(block)
-            data_left = np.concatenate([normal_data], axis=0)
-
-            generator = np.concatenate([np.identity(self.k, dtype=np.uint8), self.gf.vander], axis=0)
-            generator_left = np.delete(generator, obj=failed_i, axis=0)
-
-            data_result = self.gf.matmul(self.gf.inverse(generator_left), data_left)
-            parity_result = self.gf.matmul(self.gf.vander, data_result)
-
-            recovered_result = np.concatenate([data_result, parity_result], axis=0)
-
-            for i in failed_i:
-                roll_i = (i - stripe_i) % (self.k + self.m)
-                with open(self.path + '/node{}'.format(roll_i) + '/block{}'.format(stripe_i), 'wb') as f:
-                    block_bytes = bytes(recovered_result[i].tolist())
-                    f.write(block_bytes)
-                    f.flush()
-                    os.fsync(f.fileno())
-        return
 
     def retrieve(self, name):
         splits = []
@@ -133,3 +87,55 @@ class RAID6():
         data = np.concatenate(splits, axis=0)[:self.k,]
         trimmed_data = np.trim_zeros(data.flatten(), 'b')
         return trimmed_data.tobytes()
+
+    
+    def fail_node(self, node_id):
+        # introduce node and block failure by deleting the block file
+        node_path = os.path.join(self.path, 'node{}'.format(node_id))
+        if os.path.exists(node_path):
+            shutil.rmtree(node_path)
+            os.mkdir(node_path)
+            print("Node {} failed".format(node_id))
+            return node_id
+        else:
+            print("Node {} not found".format(node_id))
+            return None
+
+    
+    def handle_disk_failure(self):
+        for stripe_i in range(self.cur_stripe_index):
+            normal_data = []
+            failed_i = []
+            # Check for missing data blocks (failures)
+            for i in range(self.k + self.m):
+                roll_i = (i - stripe_i) % (self.k + self.m)
+                file_name = os.path.join(self.path, 'node{}'.format(roll_i), 'block{}'.format(stripe_i))
+                if not os.path.exists(file_name):
+                    #print("Detected failure: block {} in Node {}".format(stripe_i, roll_i))
+                    failed_i.append(i)
+                else:
+                    with open(self.path + '/node{}'.format(roll_i) + '/block{}'.format(stripe_i), 'rb') as f:
+                        block = np.asarray(bytearray(f.read()), dtype=np.uint8)
+                        normal_data.append(block)
+            data_left = np.concatenate([normal_data], axis=0)
+
+            ### Recover lost node data ###
+            # Concatenate identity matrix with Vandermonte matrix
+            generator = np.concatenate([np.identity(self.k, dtype=np.uint8), self.gf.vander], axis=0)
+            # Remove the rows corresponding to failed nodes
+            generator_left = np.delete(generator, obj=failed_i, axis=0)
+
+            # Invert the square matrix to solve for the missing node data
+            data_result = self.gf.matmul(self.gf.inverse(generator_left), data_left)
+            parity_result = self.gf.matmul(self.gf.vander, data_result)
+
+            recovered_result = np.concatenate([data_result, parity_result], axis=0)
+
+            for i in failed_i:
+                roll_i = (i - stripe_i) % (self.k + self.m)
+                with open(self.path + '/node{}'.format(roll_i) + '/block{}'.format(stripe_i), 'wb') as f:
+                    block_bytes = bytes(recovered_result[i].tolist())
+                    f.write(block_bytes)
+                    f.flush()
+                    os.fsync(f.fileno())
+        return
